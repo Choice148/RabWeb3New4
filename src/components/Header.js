3e29357da0ae828841d1421f30edf059bec315b7
Devmodaction1 <140625735+Devmodaction1@users.noreply.github.com>
2024-08-02 15:31:10 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -27,9 +27,9 @@ function Header() {
                   fixed-term crypto asset lending and borrowing through a 
                   novel financial instrument called fCash.
                 </p>
-                <p className="desc heading-L">
-                  How does Notional Work?.
-                </p>
+                // <p className="desc heading-L">
+                //   How does Notional Work?.
+                // </p>
                 <div className="action">
                   <ConnectWallet
                   className="btn-primary-1 btn-hero heading-SB connectBtn"
@@ -67,53 +67,55 @@ function Header() {
                       console.log("MetaMask is disconnected");
                     }}
                   />
-
+                  <a href="https://docs.notional.finance/notional-v3" target="_blank">
+                  <Button>View Docs</Button>
+                  </a>
                   
-                  <div className="btn-hero heading-SB" id="slect-main">
-                    <select
-                      className="select2-hidden-accessible select2"
-                      id="select2-main"
-                      aria-invalid="false"
-                      data-select2-id="select2-data-1-x6av"
-                      tabIndex="-1"
-                      aria-hidden="true"
-                    >
-                      <option value="">View Docs</option>
-                      <option value="0x1" data-select2-id="select2-data-3-rcyi">
-                        Ethereum
-                      </option>
-                      <option
-                        value="0x38"
-                        data-select2-id="select2-data-30-ir32"
-                      >
-                        Binance S
-                      </option>
-                      <option
-                        value="0x89"
-                        data-select2-id="select2-data-31-w03z"
-                      >
-                        Polygon
-                      </option>
-                      <option
-                        value="0xa86a"
-                        data-select2-id="select2-data-32-71oo"
-                      >
-                        Avalanche
-                      </option>
-                      <option
-                        value="0xfa"
-                        data-select2-id="select2-data-33-hulu"
-                      >
-                        Fantom
-                      </option>
-                      <option
-                        value="0x19"
-                        data-select2-id="select2-data-35-silk"
-                      >
-                        Cronos
-                      </option>
-                    </select>
-                  </div>
+                  // <div className="btn-hero heading-SB" id="slect-main">
+                  //   <select
+                  //     className="select2-hidden-accessible select2"
+                  //     id="select2-main"
+                  //     aria-invalid="false"
+                  //     data-select2-id="select2-data-1-x6av"
+                  //     tabIndex="-1"
+                  //     aria-hidden="true"
+                  //   >
+                  //     <option value="">View Docs</option>
+                  //     <option value="0x1" data-select2-id="select2-data-3-rcyi">
+                  //       Ethereum
+                  //     </option>
+                  //     <option
+                  //       value="0x38"
+                  //       data-select2-id="select2-data-30-ir32"
+                  //     >
+                  //       Binance S
+                  //     </option>
+                  //     <option
+                  //       value="0x89"
+                  //       data-select2-id="select2-data-31-w03z"
+                  //     >
+                  //       Polygon
+                  //     </option>
+                  //     <option
+                  //       value="0xa86a"
+                  //       data-select2-id="select2-data-32-71oo"
+                  //     >
+                  //       Avalanche
+                  //     </option>
+                  //     <option
+                  //       value="0xfa"
+                  //       data-select2-id="select2-data-33-hulu"
+                  //     >
+                  //       Fantom
+                  //     </option>
+                  //     <option
+                  //       value="0x19"
+                  //       data-select2-id="select2-data-35-silk"
+                  //     >
+                  //       Cronos
+                  //     </option>
+                  //   </select>
+                  // </div>
                 </div>
               </div>
             </div>
